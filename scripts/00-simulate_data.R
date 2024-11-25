977cfb499d0f5d46a8d2d6147f5d1b3fc11623ac
deyikong <kongdeyi0209@gmail.com>
2024-11-25 18:24:51 -0500

    Update Simulation

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,7 +1,7 @@
 #### Preamble ####
 # Purpose: Simulate data 
 # Author: Deyi Kong
-# Date: November 24th, 2024
+# Date: November 25th, 2024
 # Contact: deyi.kong@mail.utoronto.ca
 # License: MIT
 # Pre-requisites: The `tidyverse` package must be installed
@@ -15,22 +15,30 @@ library(arrow)
 set.seed(123)
 
 # define possible values to simulate data
-contract <- c("Contract 1", "Contract 2", "Contract 3", "Contract 4", "Contract 5")
-buyer <- c("Buyer A", "Buyer B", "Buyer C", "Buyer D")
+contract <- c("License/Maintenance fees", "Computer services", "Consultants", "Application software", "Production and operations")
+buyer <- c("Employment and Social Development Canada", "Global Affairs Canada", "National Defence", "Fisheries and Oceans Canada")
+supplier <- c("MICROSOFT CANADA INC.", "MICROSOFT CORPORATION", "MICROSOFT CANADA CO.")
 n <- 100
 
 # generate random start and end dates for each poll
-start_dates <- sample(seq(as.Date("2024-01-01"), as.Date("2024-12-31"), by = "day"), n, replace = TRUE)
-end_dates <- start_dates + sample(1:30, n, replace = TRUE)
+award_dates <- sample(seq(as.Date("2024-01-01"), as.Date("2024-12-31"), by = "day"), n, replace = TRUE)
+start_dates <- award_dates + sample(1:30, n, replace = TRUE)
+end_dates <- start_dates + sample(1:300, n, replace = TRUE)
 
 # create a simulated data frame of polls with various attributes
 procurement_data <- data.frame(
   contract = sample(contract, n, replace = TRUE),
   buyer = sample(buyer, n, replace = TRUE),
+  supplier = sample(supplier, n, replace = TRUE),
+  amount = sample(40000:450000, n, replace = TRUE),
+  award_date = award_dates,
   start_date = start_dates,
-  end_date = end_dates
+  end_date = end_dates,
+  preparatory_phase = as.numeric(start_dates - award_dates),
+  contract_days = as.numeric(end_dates - start_dates)
 )
 
 # write the cleaned and simulated polling data to a CSV file
 write_csv(procurement_data, here::here("data/simulated_data/simulated_data.csv"))
 write_parquet(procurement_data, here::here("data/simulated_data/simulated_data.parquet"))
+

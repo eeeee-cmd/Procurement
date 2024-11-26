27c250a245c410d6ddc1ba27602b03b58b7fa106
deyikong <kongdeyi0209@gmail.com>
2024-11-26 18:40:40 -0500

    Update paper

--- scripts/07-validation.R
+++ scripts/07-validation.R
@@ -17,6 +17,24 @@ library(caret)
 cleaned_data <- read_parquet(here::here("data/analysis_data/procurement_cleaned.parquet"))
 
 # data processing
+cleaned_data <- cleaned_data %>%
+  mutate(
+    Contract = as.factor(Contract),
+    Buyer = as.factor(Buyer),
+    Supplier = as.factor(Supplier),
+    AwardDate = as.Date(AwardDate),
+    StartDate = as.Date(StartDate),
+    EndDate = as.Date(EndDate)
+  ) %>%
+  mutate(PollRecency = as.numeric(Sys.Date() - StartDate))
+  
+
+# split the data into training (70%) and testing (30%) sets
+set.seed(123) # Set seed for reproducibility
+train_index <- createDataPartition(cleaned_data$CandidateName, p = 0.7, list = FALSE)
+train_data <- cleaned_data[train_index, ]
+test_data <- cleaned_data[-train_index, ]
+
 
 
 write_parquet(rmse_results, here::here("data/analysis_data/rmse_results.parquet"))
